7b4c320ef113aac69f4fdcb11090d4714e0eeee1
Redlif <dima_08_2000@ukr.net>
2020-04-04 23:07:38 +0300

    Refresh

--- Lab-1/app/src/main/java/com/example/lab_1/MainActivity.kt
+++ Lab-1/app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -11,56 +11,27 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    var radio1:String=""
-    var radio2:String=""
-    var radio111:Int=0
-    var radio222:Int=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        type_product.setOnCheckedChangeListener(
-            RadioGroup.OnCheckedChangeListener{group, checkedId ->
-                var radio11 = findViewById(checkedId) as RadioButton
-                radio1=radio11.text.toString()
-                radio11.isChecked=true
-                radio111=checkedId
-            }
-        )
-        company.setOnCheckedChangeListener(
-            RadioGroup.OnCheckedChangeListener{group, checkedId ->
-                 var radio22 = findViewById(checkedId) as RadioButton
-                radio2=radio22.text.toString()
-                radio22.isChecked=true
-                radio222=checkedId
-            }
-        )
     }
 
-
-    fun ok (view:View){
-
-        if (radio1!="" && radio2!=""){
-            textView.setText(radio1 +" and "+ radio2)
-             }
-        else
-            Toast.makeText(this," Choose something, please! ",Toast.LENGTH_LONG).show()
-
+    fun ok(view: View) {
+        val radio: RadioButton? = findViewById(type_product.checkedRadioButtonId)
+        val radio2: RadioButton? = findViewById(company.checkedRadioButtonId)
+        if(radio != null && radio2 != null) {
+            textView.setText(radio.text.toString() + " and " + radio2.text.toString())
+        }
+        else {
+            Toast.makeText(this,"Choose something please",Toast.LENGTH_LONG).show()
+        }
     }
-    fun cancel (view: View){
-        var bottom1=findViewById(radio111) as RadioButton
-        var bottom2=findViewById(radio222) as RadioButton
-        if(bottom1!=null ){
-            bottom1.isChecked=false
-            radio111=0
-        }
-        if(bottom2!=null){
-            bottom2.isChecked=false
-            radio222=0
-        }
-
-        radio1=""
-        radio2=""
+    fun cancel(view: View) {
         textView.setText("")
+        type_product.clearCheck()
+        company.clearCheck()
     }
+
 }
